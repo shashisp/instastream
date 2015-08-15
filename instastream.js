d138986dae0bee2999db72ce705a2803675c29be
Shashi Kumar <shashiessp@gmail.com>
2015-08-15 18:28:47 +0530

    Add basic directive and template to test

--- instastream.js
+++ instastream.js
@@ -1,4 +1,33 @@
-angular.module('shashisp.instastream', []).
-	directive('instastream', function (argument) {
+var app = angular.module("InstaStream", []);
 
-	});
+
+app.factory('instagram', ['$http', function($http){
+
+	return {
+		fetchPopular: function(callback){
+            
+            var endPoint = "https://api.instagram.com/v1/users/1364989185/media/recent/?client_id=642176ece1e7445e99244cec26f4de1f&count=4&callback=JSON_CALLBACK";
+       	     
+            $http.jsonp(endPoint).success(function(response){
+                callback(response.data);
+            });
+		}
+	}
+
+}]);
+
+
+
+app.directive('myStream',['instagram', function(instagram) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<p>{{ pics }}</p>',
+        controller: function($scope) {
+        },
+        link: function(scope) {
+        	scope.pics = []
+            scope.pics=instagram.data;
+        }
+    };
+}]);
